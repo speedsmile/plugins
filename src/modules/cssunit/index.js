8a60dbd98e46a316e13e7e25208d1ea6b5f7d5c8
weikaiwei <314467249@qq.com>
2017-12-15 20:39:14 +0800

    开发基于postcss插件修改样式中的css单位和值的计算插件

---
+++ src/modules/cssunit/index.js
@@ -0,0 +1,13 @@
+/**作为postcss的插件，依赖postcss
+ * Created by weikaiwei on 2017/12/15.
+ */
+let postcss = require('postcss'), Cssunit = require('./core');
+module.exports = postcss.plugin(require("./package.json").name, function () {
+  let args = arguments;
+  return function (css, result) {
+    let oldCssText = css.toString(),
+      cssunit = new Cssunit(...args),
+      newCssText = cssunit.parse(oldCssText);
+    result.root = postcss.parse(newCssText);
+  };
+});
