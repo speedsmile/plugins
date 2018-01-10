670a5d5a485194e76314517fbb157c762f62e39e
admin <314467249@qq.com>
2018-01-10 20:24:32 +0800

    webpack入口工具模块增加glob模块用以对文件路径进行匹配

--- src/plugin/entry.js
+++ src/plugin/entry.js
@@ -1,27 +1,18 @@
 /**
  * Created by weikaiwei on 2017/9/21.
  * 扫描入口文件的规则
- * 双下划线“__”开头的文件夹和文件都从入口文件排除
  */
 module.exports = function (entry, config) {
-    var fs = require("fs"), path = require("path"), base = config && config.base || "", obj = {};
-    base = base.replace(/^(\.?[\\/])|(\.[\\/])?/, "").replace(/[\\/]$/, "").replace(/\\/g, "/");
-    readDirSync(entry);
-    function readDirSync(filepath) {
-        let basename = path.basename(filepath);
-        if (!basename.startsWith("__")) {
-            var pa = fs.readdirSync(filepath);
-            pa.forEach(function (filename) {
-                var relpath = path.join(filepath, filename), info = fs.statSync(relpath);
-                if (info.isDirectory()) {
-                    console.log("dir: " + relpath);
-                    readDirSync(relpath);
-                } else if(!filename.startsWith("__")){
-                    let refname = relpath.replace(/[\/]??(\..*)?$/, "").replace(/\\/g, "/"); // 去掉模块的后缀名
-                    obj[refname.replace(new RegExp("^(\\./)?" + base + "/"), "")] = "./" + refname;
-                }
-            })
-        }
+  var path = require("path"), sep = path.sep, fs = require("fs"),
+    base = config && config.base && config.base.toString() || "", obj = {};
+  require("glob").sync(entry).forEach(function (relpath) {
+    var info = fs.statSync(relpath);
+    if (info.isFile()) {
+      let refname = relpath.replace(/\.[^.]*$/, ""), // 去掉模块的后缀名
+        // 编译后的模块路径。去掉基于base的那部分
+        key = refname.replace(new RegExp("^(\\.?" + sep + ")?" + base + sep + "?"), "");
+      obj[key] = "." + sep + refname;
     }
-    return obj;
+  });
+  return obj;
 };
