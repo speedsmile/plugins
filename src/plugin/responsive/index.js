a4c29c7edb8ba68c3d92fe1f62408ec15854e56e
weikaiwei <314467249@qq.com>
2018-03-04 21:00:10 +0800

    responsive添加自适应过程中的回调事件，用以调整实际大小和使用大小

--- src/plugin/responsive/index.js
+++ src/plugin/responsive/index.js
@@ -3,8 +3,8 @@
  * @param Object {width: 设计稿的宽度, height: 设计稿的高度, scale: 1（默认）,等比例缩放}
  *
  * */
-module.exports = function({width, height, minWidth, maxWidth, scale = 1}){
-  var baseWidth = width; //window.innerWidth < window.innerHeight ? baseWidth : baseHeight
+module.exports = function({width, height, minWidth, maxWidth, scale = 1, onResize}){
+  let baseWidth = width;
   function setBase(){
     /**基准 * 页面宽度 / 设计稿宽度
      * 设计稿： 750 * 1334
@@ -19,15 +19,16 @@ module.exports = function({width, height, minWidth, maxWidth, scale = 1}){
      * 兼容：使用华为浏览器，第一次打开标签加载网页时，window.innerWidth和window.innerHeight不准确，即使延时处理也不能确定具体的时间
      * document.body.clientWidth是准确的
      * */
-    var rootEl = (document.documentElement || document.body),
-      useWidth = window.innerWidth, useHeight, rem;
+    let rootEl = (document.documentElement || document.body),
+      fullWidth = window.innerWidth, fullHeight = window.innerHeight,
+      useWidth = fullWidth, useHeight, rem;
     /**为了保证实际尺寸的比例和理想尺寸的比例一致，以实际宽高中较小的值最为基数，按比例计算出另一个基准数
      * 以宽度/高度为基准比例，实际比例大于理想比例，说明实际的高度较小，应该根据高度反推算出宽度
      */
-    if(scale){
-      var baseRatio = baseWidth / height, terminalRatio = window.innerWidth / window.innerHeight;
+    if(scale){ // 宽高等比缩放算法
+      let baseRatio = baseWidth / height, terminalRatio = fullWidth / fullHeight;
       if(terminalRatio > baseRatio){
-        useWidth = window.innerHeight * baseRatio;
+        useWidth = fullHeight * baseRatio;
       }
       useHeight = useWidth / baseRatio
     }
@@ -39,15 +40,15 @@ module.exports = function({width, height, minWidth, maxWidth, scale = 1}){
       /**根据设计稿理想尺寸和设备实际尺寸的比例，等比计算出理想尺寸所对应的设备尺寸
        * */
       responseSize: function(size, option){
-        var {reverse, max, min} = option || {},
-          size = reverse ? size * (baseWidth / useWidth) : size / (baseWidth / useWidth);
+        let {reverse, max, min} = option || {};
+        size = reverse ? size * (baseWidth / useWidth) : size / (baseWidth / useWidth);
         max != undefined && (size = Math.min(size, max)); // 设置了最大尺寸
         min != undefined && (size = Math.max(size, min)); // 设置了最小尺寸
         return size;
       }
     };
-    return {useWidth}
+    onResize && onResize({width: useWidth, height: useHeight, fullWidth, fullHeight});
   }
   window.addEventListener("resize", setBase);
-  return setBase()
+  setBase();
 };
