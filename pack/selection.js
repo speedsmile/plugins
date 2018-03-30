15e9c29a431ee85cb12512064ca4cc89dc9f0ed9
weikaiwei <314467249@qq.com>
2018-03-30 10:33:03 +0800

    删除不必要的打包文件

--- pack/selection.js
+++
@@ -1,5 +0,0 @@
-import Vue from "vue";
-import selection from "@/components/selection";
-import option from "@/components/selection/option";
-Vue.component(selection.name, selection);
-Vue.component(option.name, option);
--- start.bat
+++
@@ -1,1 +0,0 @@
-start pack.bat
