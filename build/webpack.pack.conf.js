defb12f55ec00e0e4e76c4cd3262fb461d8d8be7
weikaiwei1 <weikaiwei1@jd.com>
2018-06-16 12:02:01 +0800

    新增打包用的webpack配置文件

---
+++ build/webpack.pack.conf.js
@@ -0,0 +1,96 @@
+var path = require('path')
+var utils = require('./utils')
+var webpack = require('webpack')
+var config = require('../config')
+var vueLoaderConfig = require('./vue-loader.conf')
+let property = require("../config/property")
+var Entry = require('../src/plugin/entry');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
+console.log(utils.assetsPath('img/[name].[hash:7].[ext]'))
+var entry = Entry("test/*/*.js", {base: "test", template: {output: resolve("dist/") + require("../package.json").name}});
+
+module.exports = {
+  // entry: entry.js,
+  entry: {
+    "VForm.cmd": "./src/plugin/VForm/VForm.cmd"
+    // "Selection.cmd": "./src/components/selection/cmd"
+  },
+  output: {
+    path: config.build.assetsRoot,
+    filename: '[name].js',
+    publicPath: process.env.NODE_ENV === 'production'
+      ? config.build.assetsPublicPath
+      : config.dev.assetsPublicPath
+  },
+  resolve: {
+    extensions: ['.js', ".vue", ".less", ".css"],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': resolve('src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: [
+          {
+            loader: 'vue-loader',
+            options: vueLoaderConfig
+          },
+          {
+            loader: 'iview-loader',
+            options: {
+              prefix: false
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src'), resolve('test'), resolve('node_modules/iview/src')]
+      },
+      ...utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, extract: true}),
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("selection.css"),
+    //...entry.html.map(html => new HtmlWebpackPlugin(html))
+    // new webpack.ProvidePlugin({
+    //   $: "jquery",
+    //   jQuery: "jquery"
+    // })
+  ],
+  externals: {
+  }
+}
